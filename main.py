e1d55a049ca47b71bba998fc21449365fbac925e
Pramista KC <kcpramista@Pramistas-MacBook-Pro.local>
2025-10-18 10:42:39 +0900

    Create initial bubble and move them

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
-import pygame # type : ignore
+import pygame
+import sys
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
+#DEFINITIONS
+BLUE = (0, 0, 255)
+SKY_BLUE = (85, 156, 195)
+
+# sizes
+screen_width, screen_height = 720, 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+bubbles = []
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("Blue Canvas")
+
+running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:   #X button
             running = False
-    
-    screen.fill("blue")
 
+
+    #bubbles
+    if random.randint(1,60) == 1:
+        x_pos = random.randint(0, screen_width)
+        radius = random.randint(10,30)
+        speed = random.uniform(0.5, 2.5)
+        new_bubble = {
+            'x': x_pos,
+            'y': screen_height + radius,
+            'radius': radius,
+            'speed': speed
+        }
+        bubbles.append(new_bubble)
+
+    #bubble are going up
+    for bubble in bubbles:
+        bubble['y'] -= bubble['speed']
+
+    #removing bubbles that go off the screen
+    bubbles = [bubble for bubble in bubbles if bubble['y'] > -bubble['radius']]
+
+    #drawing
+    screen.fill(BLUE)
+    for bubble in bubbles:
+        pygame.draw.circle(screen, SKY_BLUE, (bubble['x'], bubble['y']), bubble['radius'])
+    
     pygame.display.flip()
 
-    clock.tick(60)
+    clock.tick(100)
 
 pygame.quit()
+sys.exit()
